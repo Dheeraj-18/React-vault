c0e21c9b5d380a93016a8122374c89895d382ea2
think2code <dheerajsaraswat03@gmail.com>
2025-11-03 16:29:43 +0530

    nested data fetching using Promise.all()

---
+++ 22_nested-data-feteching/components/CountryCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function CountryCard(props) {
+  const { name, flag , population , region ,capital } = props
+  return (
+    <Link to={`/${name}`} className="country-card">
+      <img src={flag} alt={name + 'flag'} />
+      <div className="card-text">
+        <h3 className="card-title">{name}</h3>
+        <p>
+          <b>Population: </b> {population}
+        </p>
+        <p>
+          <b>Region: </b>{region}
+        </p>
+        <p>
+          <b>Capital: </b>{capital}
+        </p>
+      </div>
+    </Link>
+  )
+}
