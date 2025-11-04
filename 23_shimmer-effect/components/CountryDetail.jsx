aead894ba785beb16fbf2f5c7df43c6e6ed23c1b
think2code <dheerajsaraswat03@gmail.com>
2025-11-04 09:45:01 +0530

    ref - refector component

--- 23_shimmer-effect/components/CountryDetail.jsx
+++ 23_shimmer-effect/components/CountryDetail.jsx
@@ -59,66 +59,64 @@ export default function CountryDetail() {
   return countryData === null ? (
     'loading...'
   ) : (
-    <>
-      <main>
-        <div className="country-details-container">
-          <span className="back-button" onClick={() => history.back()}>
-            <i className="fa-solid fa-arrow-left"></i>&nbsp;&nbsp; Back
-          </span>
-          <div className="country-details">
-            <img src={countryData.flag} alt={`${countryData.name} flag`} />
-            <div className="details-text-container">
-              <h1>{countryData.name}</h1>
-              <div className="details-text">
-                <p>
-                  <b>Native Name: {countryData.nativeName}</b>
-                  <span className="native-name"></span>
-                </p>
-                <p>
-                  <b>
-                    Population: {countryData.population.toLocaleString('en-In')}
-                  </b>
-                  <span className="population"></span>
-                </p>
-                <p>
-                  <b>Region: {countryData.region} </b>
-                  <span className="region"></span>
-                </p>
-                <p>
-                  <b>Sub Region:{countryData.subregion} </b>
-                  <span className="sub-region"></span>
-                </p>
-                <p>
-                  <b>Capital: {countryData.capital.join(' ')} </b>
-                  <span className="capital"></span>
-                </p>
-                <p>
-                  <b>Top Level Domain:{countryData.tld} </b>
-                  <span className="top-level-domain"></span>
-                </p>
-                <p>
-                  <b>Currencies:{countryData.currencies} </b>
-                  <span className="currencies"></span>
-                </p>
-                <p>
-                  <b>Language: {countryData.languages} </b>
-                  <span className="languages"></span>
-                </p>
-              </div>
-              {countryData.borders.length !== 0 && (
-                <div className="border-countries">
-                  <b>Border Countries: </b>&nbsp;
-                  {countryData.borders.map((border) => (
-                    <Link key={border} to={`/${border}`}>
-                      {border}
-                    </Link>
-                  ))}
-                </div>
-              )}
+    <main>
+      <div className="country-details-container">
+        <span className="back-button" onClick={() => history.back()}>
+          <i className="fa-solid fa-arrow-left"></i>&nbsp;&nbsp; Back
+        </span>
+        <div className="country-details">
+          <img src={countryData.flag} alt={`${countryData.name} flag`} />
+          <div className="details-text-container">
+            <h1>{countryData.name}</h1>
+            <div className="details-text">
+              <p>
+                <b>Native Name: {countryData.nativeName}</b>
+                <span className="native-name"></span>
+              </p>
+              <p>
+                <b>
+                  Population: {countryData.population.toLocaleString('en-In')}
+                </b>
+                <span className="population"></span>
+              </p>
+              <p>
+                <b>Region: {countryData.region} </b>
+                <span className="region"></span>
+              </p>
+              <p>
+                <b>Sub Region:{countryData.subregion} </b>
+                <span className="sub-region"></span>
+              </p>
+              <p>
+                <b>Capital: {countryData.capital.join(' ')} </b>
+                <span className="capital"></span>
+              </p>
+              <p>
+                <b>Top Level Domain:{countryData.tld} </b>
+                <span className="top-level-domain"></span>
+              </p>
+              <p>
+                <b>Currencies:{countryData.currencies} </b>
+                <span className="currencies"></span>
+              </p>
+              <p>
+                <b>Language: {countryData.languages} </b>
+                <span className="languages"></span>
+              </p>
             </div>
+            {countryData.borders.length !== 0 && (
+              <div className="border-countries">
+                <b>Border Countries: </b>&nbsp;
+                {countryData.borders.map((border) => (
+                  <Link key={border} to={`/${border}`}>
+                    {border}
+                  </Link>
+                ))}
+              </div>
+            )}
           </div>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   )
 }
