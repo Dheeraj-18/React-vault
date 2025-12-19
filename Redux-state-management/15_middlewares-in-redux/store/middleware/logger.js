5e80fa775015adfd0d0b7e604b4df97c4cf38d4a
think2code <dheerajsaraswat03@gmail.com>
2025-12-19 15:51:13 +0530

    doc - comment note of middleware

--- Redux-state-management/15_middlewares-in-redux/store/middleware/logger.js
+++ Redux-state-management/15_middlewares-in-redux/store/middleware/logger.js
@@ -4,3 +4,7 @@ export const logger = (store) => (next) => (action) => {
   console.log('action: ', action)
   next(action)
 }
+
+// What is Redux Middleware? (Plain definition)
+
+// Redux middleware is a function that sits between dispatch and reducer and lets you intercept, modify, delay, or stop actions.
